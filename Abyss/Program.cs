2ca3c6584c0749b2468983dc0851b56c241800c5
Xwilarg <xwilarg@yahoo.fr>
2021-02-18 23:01:11 +0100

    Add empty Discord bot

---
+++ Abyss/Program.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordUtils;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Abyss
+{
+    public class Program
+    {
+        public static void Main()
+               => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public DiscordSocketClient Client { private set; get; }
+        private readonly CommandService _commands = new CommandService();
+
+        private Program()
+        {
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+            });
+            Client.Log += Utils.Log;
+            _commands.Log += Utils.LogErrorAsync;
+        }
+
+        private async Task MainAsync()
+        {
+            var json = JsonConvert.DeserializeObject<JObject>(File.ReadAllText("Keys/Credentials.json"));
+            if (json["botToken"] == null)
+                throw new NullReferenceException("Invalid Credentials file");
+
+            Client.MessageReceived += HandleCommandAsync;
+
+            await Client.LoginAsync(TokenType.Bot, json["botToken"].Value<string>());
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage msg = arg as SocketUserMessage;
+            if (msg == null || arg.Author.IsBot) return;
+            int pos = 0;
+            if (msg.HasMentionPrefix(Client.CurrentUser, ref pos) || msg.HasStringPrefix("#", ref pos))
+            {
+                SocketCommandContext context = new SocketCommandContext(Client, msg);
+                await _commands.ExecuteAsync(context, pos, null);
+            }
+        }
+    }
+}
